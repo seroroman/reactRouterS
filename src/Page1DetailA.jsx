ea36e239b0333d0182978b5750f2603911cbdc4d
seroroman <47804854+seroroman@users.noreply.github.com>
2022-02-01 12:00:46 +0900

    useHistory

--- src/Page1DetailA.jsx
+++ src/Page1DetailA.jsx
@@ -1,9 +1,15 @@
-import { useLocation } from "react-router-dom";
+import { useLocation, useHistory } from "react-router-dom";
 export const Page1DetailA = () => {
   const { state } = useLocation();
+  const history = useHistory();
+  console.log(history);
+  const onClickBack = () => {
+    history.goBack();
+  };
   return (
     <div>
       <p>Page1DetailAです</p>
+      <button onClick={onClickBack}>戻る</button>
     </div>
   );
 };
